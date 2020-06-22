cb44442819c0823ff7a34e15b0545582d7853a4a
armoz14 <38024713+armorasha@users.noreply.github.com>
2020-06-22 21:02:57 +0930

    version.php complete

--- mysqli_test.php
+++
@@ -1,6 +0,0 @@
-<?php
-error_reporting(E_ALL);
-$conn = new mysqli('127.0.0.1', 'root', 'mysql', 'php_pdo', 3306);
-$result = $conn->query("SELECT NOW()");
-$now = $result->fetch_row()[0];
-echo "$now\n";
