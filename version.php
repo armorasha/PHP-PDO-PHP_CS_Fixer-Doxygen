caba94517d85c4597ec827b48315dffedba99132
armoz14 <38024713+armorasha@users.noreply.github.com>
2020-06-22 20:41:05 +0930

    version.php complete

--- version.php
+++ version.php
@@ -4,10 +4,15 @@ $dsn = 'mysql:host=localhost;dbname=php_pdo';
 $user = 'root';
 $password = 'mysql';
 
+// creating new PDO
 $pdo = new PDO($dsn, $user, $password);
 
+// using the PDO, a query is being made and saved to $smt
 $stm = $pdo->query('SELECT VERSION()');
 
+//The PDO statement's fetch() method fetches the next row from a result set. 
+// In our case it is a version of MySQL.
 $version = $stm->fetch();
 
+//The $version is an array, we get its first value.
 echo 'MySQL version being used is ' . $version[0] . PHP_EOL;
